73d4fa51ba60be0978030e988e1391f5ce693099
GKastet <127600582+GKastet@users.noreply.github.com>
2024-01-15 12:04:19 +0100

    next

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App basename={import.meta.env.BASE_URL}/>
-    {/* <App /> */}
-  </React.StrictMode>,
-)
-
 // ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>    
-//     <App />
+//   <React.StrictMode>
+//     <App basename={import.meta.env.BASE_URL}/>
+//     {/* <App /> */}
 //   </React.StrictMode>,
 // )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>    
+    <App />
+  </React.StrictMode>,
+)
